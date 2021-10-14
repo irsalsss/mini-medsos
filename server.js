9875f8892a46ea216979e0b786a5e0d1ee83508d
irsalsss <muhamadirsal@gmail.com>
2021-10-14 07:30:57 +0700

    fix history api fallback

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var path = require('path');
 var express = require('express');
+const history = require('connect-history-api-fallback')
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(history())
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
